7eff345454afd2ed2c64f7823fffbeeea4f50828
sarahbrande <48233952+sarahbrande@users.noreply.github.com>
2019-03-12 22:27:35 -0600

    Add files via upload

---
+++ tut8b.pl
@@ -0,0 +1,33 @@
+# 
+#  Findin out what you matched
+#
+#
+#
+#
+#
+#
+#
+#
+use strict;                                               #forces variable declarations
+use warnings;     
+$|=1;                                                     #turns off buffering to terminal on linux/unix systems
+sub main 
+{ 
+	my $input_file='C:\Users\sarah\Desktop\Udemy\PerlCourse\Seuss.txt'; # found a text file of Seuss books.
+	open(INPUT,$input_file) or die("Input file $input_file not found"); #
+	#my $outputfile = 'output.txt';                                      #
+	#open(OUTPUT,'>'.$outputfile) or die "can't create output.";         #
+ 	while(my $line = <INPUT>) {
+		#		if($line =~ /gr..n/) {                                      #
+		if($line =~ /(gr..n)(e..)(...)/) {                                      #
+		print "$1 $2 $3\n";
+		print "first match: '$1' ;second match:'$2' $3\n";
+                  }													
+		}
+	close(INPUT);
+
+	#	close(OUTPUT);
+	close(INPUT);
+}
+
+main();
