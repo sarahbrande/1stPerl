801c2665124258fb46c358f67d56749c5db03b50
sarahbrande <48233952+sarahbrande@users.noreply.github.com>
2019-07-05 01:05:29 -0600

    Add files via upload

---
+++ tut16a.pl
@@ -0,0 +1,30 @@
+#get rid of new lines and spaces
+use strict;
+use warnings;
+use Data::Dumper;
+$|=1;
+
+sub main {
+   my $input = 'tut16.csv';
+   unless(open(INPUT, $input)) {
+          die "\n Cannot open $input\n"
+   }
+   <INPUT>;  # gets rid of header,<**> reads a record (which default is a line)
+   my @lines;
+   my $count=0;
+   while(my $line =<INPUT>) {
+          chomp $line;  #you can set record separator, default is newline.
+	  # chomp removes trailing record separators. 
+	  print "'$line'\n";
+	  # my @values = split ',', $line;  
+	  my @values = split /\s*,\s*/, $line;  #this regex will get rid of spaces 
+	  $lines[$count]=$line;
+          $count++;
+  }
+  close INPUT;
+  foreach my $line(@lines){
+	  print $line.  "\n";
+  }
+
+}
+main();
